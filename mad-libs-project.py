ec5fdcd5c53bfeec59788ccf30db067857475e71
tagasatryan11-rgb <tagasatryan11@gmail.com>
2026-02-18 19:23:41 +0400

    mad-libs-project

---
+++ mad-libs-project.py
@@ -0,0 +1,26 @@
+def space_story():
+    transport = "Executor"
+    name = "Darth Vader"
+    noun = "rubber duck"
+    planet = "Mustafar"
+    adjective = "glowing"
+    verb1 = "dance"
+    verb2 = "run"
+
+    story = f"""
+Yesterday I flew to space on a {transport}.
+With me was {name}, who brought a {noun}.
+On planet {planet}, we met a {adjective} alien.
+It asked us to {verb1}, but we decided to {verb2} and return home.
+"""
+
+    print("Space Adventure Story:")
+    print(story)
+
+
+def main():
+    space_story()
+
+
+if __name__sss == "__main__":
+    main()
